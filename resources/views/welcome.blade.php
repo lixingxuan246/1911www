17fc282f92f645917cad45e39367be214037e2e9
lixingxuan <1171778420@qq.com>
2020-08-01 11:50:41 +0800

    wwww

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    www
+                    wwww
                 </div>
 
                 <div class="links">
